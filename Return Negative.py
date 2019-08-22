1a55d44592f0385eeda9f0c63388d55f306adc82
Artur Apanasov <a.apanasov@invento.by>
2019-08-22 14:27:45 +0300

    Return Negative

---
+++ Return Negative.py
@@ -0,0 +1,29 @@
+"""
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Example:
+
+make_negative(1);  # return -1
+make_negative(-5); # return -5
+make_negative(0);  # return 0
+
+Notes:
+
+    The number can be negative already, in which case no change is required.
+    Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+"""
+
+
+def make_negative_1(number):
+    if number <= 0:
+        return number
+    else:
+        return number * -1
+
+
+def make_negative_2(number):
+    return -abs(number)
+
+
+def make_negative_3(number):
+    return -number if number > 0 else number
